34e33cb8ec83d30bd6046c82e221edc512282147
K-KY <kyuyoungk@naver.com>
2025-08-11 16:17:24 +0900

    Initial Commit

---
+++ src/main/java/com/java/AI_Guardian/AiGuardianApplication.java
@@ -0,0 +1,13 @@
+package com.java.AI_Guardian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiGuardianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiGuardianApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/AI_Guardian/AiGuardianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.AI_Guardian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiGuardianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
